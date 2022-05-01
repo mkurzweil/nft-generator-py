01b472336018ddc701d449eee0b6e46c7f063d46
mkurzweil <MarioKurzweil13@gmail.com>
2022-05-01 18:39:05 +0200

    - delete backgrounds before new generation

--- generate_png.py
+++ generate_png.py
@@ -1,10 +1,16 @@
 import os
+import glob
 import random
 
 from PIL import Image
 from config import * # variable and path config
-from png import *
-from png import *
+from duplicate_check_png import *
+from duplicate_check_png import *
+
+# delete backgrounds from folder
+files = glob.glob('trait-layers/backgrounds/collection/*')
+for f in files:
+    os.remove(f)
 
 for i in range(number_of_png):
     # generate random numbers
@@ -18,7 +24,7 @@ for i in range(number_of_png):
     img = Image.new( mode = "RGB", size = (width, height), color = (r, g, b) )
     img.save(os.path.join(out_dir, 'background_' + str(i) + '.png'))
 
-    # TODO: comparison not working
+    # TODO: comparison not working.
     duplication_check()
 
     if i == number_of_png-1:
@@ -35,10 +41,13 @@ for i in range(number_of_png):
 layers_values += '],'
 layers_filename += '],'
 layers_weight += ']'
-
+print('\n')
 # copy output to index.py
+print('Just copy&paste to index.py:')
+print('{')
 print('"name": "Background",')
 print(layers_values)
 print('"trait_path": "./trait-layers/backgrounds",')
 print(layers_filename)
 print(layers_weight)
+print('}')
